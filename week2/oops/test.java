d489e23d7bfed531ebb34825d5c5377f79d18223
Shreyansh00001 <152704171+Shreyansh00001@users.noreply.github.com>
2026-06-13 14:03:03 +0530

    week 2 oops done

---
+++ week2/oops/test.java
@@ -0,0 +1,26 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        
+        Veena veena = new Veena();
+        veena.play();
+        
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+        
+        System.out.println("--- Demonstrating Polymorphism ---");
+        
+        Playable playableRef;
+        
+        playableRef = veena;
+        playableRef.play(); 
+        
+        playableRef = saxophone;
+        playableRef.play(); 
+}
+}
---
+++ week2/oops/veena.java
@@ -0,0 +1,10 @@
+package music.string;
+
+import music.Playable;
+
+public class Veena implements Playable {
+    @Override
+    public class play() {
+        System.out.println("Playing the Veena: Rich, classical string vibrations.");
+    }
+}
